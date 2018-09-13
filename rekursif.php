4f8fe98cc920743bc646d11419b07666fa3f2141
daniazalfa <42663124+daniazalfa@users.noreply.github.com>
2018-09-13 08:58:40 +0700

    Update rekursif.php

--- rekursif.php
+++ rekursif.php
@@ -1,10 +1,29 @@
-<?php
+<?php 
 
-function rekursif($i){
-  echo "$i <br>";
-  $i--;
-  if($i>0)
-    rekursif($i);
-}
-rekursif(5);
-?>
+$bnykpiramida= 5	;
+    printPiramida(0);
+	 function printPiramida($a){
+	  global $bnykpiramida;
+	  printSpace(0,$a);
+	  printStar($bnykpiramida,$a);
+	  echo "<br>";
+	if (++$a <$bnykpiramida);
+	  printPiramida($a);
+    }
+	 function printSpace($b,$a){
+		global $bnykpiramida;
+	if ($b > $bnykpiramida - $a + 0){
+	echo "&nbsp;";
+	printSpace($b + 1, $a);
+	}
+	}
+	 function printStar($z,$a){
+		global $bnykpiramida;
+		if ($z > $bnykpiramida - $a ){
+		echo "$z" ;
+		echo "&nbsp";
+		printStar($z - 1,$a);
+		}
+	}
+
+ ?>
